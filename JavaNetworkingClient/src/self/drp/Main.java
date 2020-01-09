e4c142b25e9b5d5a63103bce5e8d8d61fdf50f08
Dustin Peterson <dustinr.peterson@gmail.com>
2020-01-09 02:58:24 -0600

    Completed.  Added ability to read text file on client and write to server

--- JavaNetworkingClient/src/self/drp/Main.java
+++ JavaNetworkingClient/src/self/drp/Main.java
@@ -9,14 +9,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String textFile = "src/self/drp/temp.txt";
+
 
         try {
             Socket socket = new Socket("127.0.0.1", 4415);
-            System.out.println("Connected");
+            System.out.println("Connected to server");
             DataInputStream in = new DataInputStream(System.in);
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            out.writeUTF("Hello Server");
+            //out.writeUTF("Hello Server");
 
+            FileReader fileReader = new FileReader(textFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                out.writeUTF(line);
+            }
+
+            fileReader.close();
 
 
         } catch (IOException e) {
